a64a652b03c4c52c3c7406bf63b42d9af27b5e05
Maurice <mauriceprivat98@gmail.com>
2019-05-05 22:10:38 +0200

    Adds first account creation step

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { GoogleLoginService } from 'src/app/services/GoogleLoginService/google-login.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  imageUrl: string="";
+  countryList: {Code: string, Name: string};
+  selCountry: string="";
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder, private gLoginService: GoogleLoginService,
+    private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('assets/data/countrylist.json').subscribe((countryList: {Code: string, Name: string}) => {
+      this.countryList = countryList;
+    })
+
+    this.gLoginService.getSignedInUser().then((user: gapi.auth2.GoogleUser) => {
+      this.imageUrl = user.getBasicProfile().getImageUrl();
+    });
+
+    this.firstFormGroup = this._formBuilder.group({
+      username: ['', Validators.required],
+      birthday: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
 
 }
--- src/app/components/login-page/login-page.module.ts
+++ src/app/components/login-page/login-page.module.ts
@@ -3,14 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { LoginPageRoutingModule } from './login-page-routing.module';
 import { LoginPageComponent } from './login-page.component';
+import { MatStepperModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule, MatSelectModule, MatIconModule } from '@angular/material';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    LoginPageComponent
+    LoginPageComponent,
   ],
   imports: [
     CommonModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginPageModule { }
