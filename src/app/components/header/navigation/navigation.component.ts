0b293c7fc4bc7cb05027563ac9173a1e87d99af0
Maurice <mauriceprivat98@gmail.com>
2019-05-05 16:38:20 +0200

    Fixes header items active indication

--- src/app/components/header/navigation/navigation.component.ts
+++ src/app/components/header/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    
+    this.router.events.subscribe((routerEvent) => {
+      if(routerEvent instanceof NavigationEnd) {
+        let allHeaderElements = document.getElementsByClassName("gp_main_header_item");
+        for(let i=0; i<allHeaderElements.length; i++){
+          allHeaderElements[i].classList.remove("active")
+        }
+        let currentRoute: string = this.router.url;
+        document.getElementById(`gp_${currentRoute.substr(1)}`).classList.add("active")
+      }
+    });
   }
 
 }
