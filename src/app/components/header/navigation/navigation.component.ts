bf14e93dd23b47ad4ba3f5dd34be8efb43af7870
Maurice <mauriceprivat98@gmail.com>
2019-05-08 19:19:11 +0200

    Changes how routing highlights work

--- src/app/components/header/navigation/navigation.component.ts
+++ src/app/components/header/navigation/navigation.component.ts
@@ -11,19 +11,6 @@ export class NavigationComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
-    
-    this.router.events.subscribe((routerEvent) => {
-      if(routerEvent instanceof NavigationEnd) {
-        let allHeaderElements = document.getElementsByClassName("gp_main_header_item");
-        for(let i=0; i<allHeaderElements.length; i++){
-          allHeaderElements[i].classList.remove("active")
-        }
-        let currentRoute: string = this.router.url;
-        let currentRouteHeaderItem = document.getElementById(`gp_${currentRoute.substr(1)}`);
-        if(currentRouteHeaderItem != null)
-          currentRouteHeaderItem.classList.add("active");
-      }
-    });
   }
 
 }
