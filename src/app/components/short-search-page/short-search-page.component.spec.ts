f84837b3ba32672cf4e73aa24de27fcc47a24224
Maurice <mauriceprivat98@gmail.com>
2019-07-29 01:29:16 +0200

    Fixes test

--- src/app/components/short-search-page/short-search-page.component.spec.ts
+++ src/app/components/short-search-page/short-search-page.component.spec.ts
@@ -14,6 +14,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('ShortSearchPageComponent', () => {
   let component: ShortSearchPageComponent;
@@ -36,6 +37,7 @@ describe('ShortSearchPageComponent', () => {
         MatToolbarModule,
         MatExpansionModule,
         MatButtonModule,
+        MatSnackBarModule,
         HttpClientTestingModule
       ]
     })
